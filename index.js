8b6d0838551082e4c7050d599cd288ffce434106
ranan <ranan@email.com>
2023-09-27 08:52:26 +0800

    将光标闪烁单独提取出来

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-// el：挂载元素
-// content：打印元素，要求为一个数组
 function main(options) {
-    let {el, content, typeInterval, backInterval, pauseDelay, cursorInterval} = options
-    if (!typeInterval || Number.isNaN(Number(typeInterval))) typeInterval = 300 // 打字间隔
-    if (!backInterval || Number.isNaN(Number(backInterval))) backInterval = 200 // 回退间隔
-    if (!pauseDelay || Number.isNaN(Number(pauseDelay))) pauseDelay = 0 // 停顿时间
-    if (!cursorInterval || Number.isNaN(Number(cursorInterval))) cursorInterval = 200 // 光标闪烁间隔
+    let { el, content, typeInterval, backInterval, pauseDelay, cursorInterval } = options
     if (typeof el != "string") {
         console.error("el is required String")
         return
@@ -14,35 +8,43 @@ function main(options) {
         console.error("content is required Array")
         return
     }
+    if (!typeInterval || Number.isNaN(Number(typeInterval))) typeInterval = 300 // 打字间隔
+    if (!backInterval || Number.isNaN(Number(backInterval))) backInterval = 200 // 回退间隔
+    if (!pauseDelay || Number.isNaN(Number(pauseDelay))) pauseDelay = 0 // 停顿时间
+    if (!cursorInterval || Number.isNaN(Number(cursorInterval))) cursorInterval = 200 // 光标闪烁间隔
 
     let i = 0 // content数组的索引
     let num = 0 // 目前打印的字体数
     let timer = null; // 定时器
-    let flashFlag = true; // 控制光标闪烁
 
     let typed = document.querySelector(el);
 
     typing()
+    cursorFlash(".cursor")
+
     // 打字效果
     function typing() {
+        let delayTimer = null
         timer = setInterval(() => {
-            print(content[i])
+            print()
             num++
             if (num > content[i].length) {
                 num = content[i].length
-
-                let delayTimer = setTimeout(() => {
-                    clearInterval(timer)
-                    backspace()
-                    clearTimeout(delayTimer)
-                }, pauseDelay)
+                if (!delayTimer) {
+                    delayTimer = setTimeout(() => {
+                        clearInterval(timer)
+                        backspace()
+                        clearTimeout(delayTimer)
+                    }, pauseDelay)
+                }
             }
         }, typeInterval)
     }
+
     // 退格效果
     function backspace() {
         timer = setInterval(() => {
-            print(content[i])
+            print()
             num--
             if (num < 0) {
                 num = 0
@@ -55,27 +57,26 @@ function main(options) {
             }
         }, backInterval)
     }
+
     // 光标闪烁效果
-    // element：元素
-    // flag：用于控制光标闪烁
-    function cursorFlash(element, flag) {
-        let cursor = document.querySelector(element)
-        let timer = setTimeout(() => {
+    // el：元素
+    function cursorFlash(el) {
+        let cursor = document.querySelector(el)
+        let flag = true
+        let interval = setInterval(() => {
             if (flag) {
                 cursor.style.opacity = "0"
                 flag = false
-                clearTimeout(timer)
             } else {
                 cursor.style.opacity = "1"
                 flag = true
-                clearTimeout(timer)
             }
         }, cursorInterval)
-        return flag
     }
 
-    function print(content) {
-        typed.innerHTML = `<span>${content.slice(0, num)}</span><span class="cursor"></span>`;
-        flashFlag = cursorFlash(".cursor", flashFlag)
+    function print() {
+        typed.innerText = `${content[i].slice(0, num)}`;
     }
 }
+
+export default main
